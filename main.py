39a3eaaedd929cc6626617e656afeaa7739439b5
M3g4lodon <mathieu.seris@student.ecp.fr>
2017-11-24 12:06:55 +0100

    New files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+script_dir = os.getcwd()
+relative_location="/Data/CACM/cacm.all"
+file_location = script_dir+relative_location
+
+def read_doc():
+    file=open(file_location,"r+")
+    collection=file.read()
+    file.close()
+    while
+
+if __name__=="__main__":
+    read_doc()
