4acb7da898620479073b2a2eae9061ed4070ef7e
SoleneDc <duchamp.solene.pro@gmail.com>
2018-02-08 11:28:39 +0100

    Other weights for vectorial search2

--- IV_Recherches.py
+++ IV_Recherches.py
@@ -95,10 +95,10 @@ def norm_factor(collection, doc_id):
             return norm_factor_doc
 
 
-def vectorial_search(reversed_index, dic_documents, query, weight_tf_idf_query, weight_tf_idf_doc):
+def vectorial_search(reversed_index, dic_documents, query, weight_tf_idf_query, weight_tf_idf_doc, collection):
     nb_docs = len(dic_documents)  # nombre de documents dans la collection
     nq = 0  # représente la somme des poids au carré des termes de la query par rapport au document query
-    nd = defaultdict(int)  # facteur de normalisation de chaque documents
+    nd = defaultdict(int)  # facteur de normalisation de chaque document
     # s est le vecteur similarité s[j] est la similarité du doc j avec la query,
     # s est appelé aussi score
     s = [0 for _ in range(nb_docs + 1)]
@@ -106,10 +106,10 @@ def vectorial_search(reversed_index, dic_documents, query, weight_tf_idf_query, 
                        stemmer.stem(word) not in COMMON_WORDS]
     reversed_index_query = construction_index_query(query)  # Index inversé sur la requête
     for term in term_list_query:  # on parcourt les mots de la query
-        wq = weight_tf_idf_query(term, reversed_index_query, reversed_index, nb_docs)
+        wq = weight_tf_idf_query(term, reversed_index_query, reversed_index, nb_docs, term_list_query)
         nq += wq ** 2
         for doc_id in reversed_index[term]['tf']:
-            wt = weight_tf_idf_doc(term, doc_id, reversed_index, nb_docs)
+            wt = weight_tf_idf_doc(term, doc_id, reversed_index, nb_docs, collection)
             doc_id = int(doc_id)
             nd[doc_id] += wt ** 2
             s[doc_id] += wt * wq
@@ -118,8 +118,10 @@ def vectorial_search(reversed_index, dic_documents, query, weight_tf_idf_query, 
             s[j] = s[j] / (sqrt(nq) * sqrt(nd[j]))
     return sorted(enumerate(s), key=lambda x: -x[1])
 
+#Diverses fonctions de poids...
 
-def weight_tf_idf_query1(term, reversed_index_query, reversed_index, nb_docs):
+#1 (celui de la page 95)
+def weight_tf_idf_query1(term, reversed_index_query, reversed_index, nb_docs, term_list_query):
     if term in reversed_index:  # si le terme n'est dans aucun document, on ne le considerera pas pour la suite
         tf_td = reversed_index_query[term]['tf']
         df_t = reversed_index[term]['idf']
@@ -127,11 +129,78 @@ def weight_tf_idf_query1(term, reversed_index_query, reversed_index, nb_docs):
     else:
         return 0
 
-
-def weight_tf_idf_doc1(term, doc_id, reversed_index, nb_docs):
+def weight_tf_idf_doc1(term, doc_id, reversed_index, nb_docs, collection):
     tf_td = reversed_index[term]['tf'][doc_id]
     df_t = reversed_index[term]['idf']
     return (1 + log(tf_td)) * log(nb_docs / df_t)
+
+#2 (Première ligne)
+def weight_tf_idf_query2(term, reversed_index_query, reversed_index, nb_docs, term_list_query):
+    if term in reversed_index:  # si le terme n'est dans aucun document, on ne le considerera pas pour la suite
+        tf_td = reversed_index_query[term]['tf']
+        return tf_td
+    else:
+        return 0
+
+def weight_tf_idf_doc2(term, doc_id, reversed_index, nb_docs, collection):
+    tf_td = reversed_index[term]['tf'][doc_id]
+    return tf_td
+
+#3 (Deuxième ligne)
+def weight_tf_idf_query3(term, reversed_index_query, reversed_index, nb_docs, term_list_query):
+    weight_sum = 0
+    moy_tf = 0
+    if term in reversed_index:  # si le terme n'est dans aucun document, on ne le considerera pas pour la suite
+        for word in term_list_query:
+            tf = reversed_index_query[word]['tf']
+            df = reversed_index_query[word]['idf']
+            weight_sum += (1+log(tf))*df
+            moy_tf += tf
+        moy_tf = moy_tf/len(term_list_query)
+        weight_sum = weight_sum/(1+log(moy_tf))
+        tf_td = reversed_index_query[term]['tf']
+        df_t = reversed_index[term]['idf']
+        return (1 + log(tf_td))*df_t/(1+log(moy_tf))/weight_sum
+    else:
+        return 0
+
+def weight_tf_idf_doc3(term, doc_id, reversed_index, nb_docs, collection):
+    nd = 0
+    sum_weight = 0
+    moy_tf = 0
+
+    tf_td = reversed_index[term]['tf'][doc_id]
+    df_t = reversed_index[term]['idf']
+    list_of_terms = list_of_words(doc_id, collection)
+    if len(list_of_terms) != 0:
+        for word in list_of_terms:
+            moy_tf += reversed_index[word]['tf'][doc_id]
+            sum_weight += small_weight_tf_idf_doc3(word, doc_id, list_of_terms)
+        moy_tf = moy_tf/len(list_of_terms)
+        nd = 1/sqrt(sum_weight)
+        return (1 + log(tf_td))/(1+log(moy_tf)) * df_t * nd
+    return 0
+
+def small_weight_tf_idf_doc3(term, doc_id, list_of_terms):
+    moy_tf = 0
+    tf_td = reversed_index[term]['tf'][doc_id]
+    df_t = reversed_index[term]['idf']
+    for word in list_of_terms:
+        moy_tf += reversed_index[word]['tf'][doc_id]
+    moy_tf = moy_tf / len(list_of_terms)
+    return (1 + log(tf_td)) / (1 + log(moy_tf)) * df_t
+
+
+def list_of_words(id_doc, collection):
+    list_of_stemmed_words = []
+    for document in collection:
+        if document.id == id_doc:
+            list_of_words = document.word_lists
+            for word in list_of_words:
+                stemmed_word = stemmer.stem(word)
+                if stemmed_word not in COMMON_WORDS:
+                    list_of_stemmed_words += [stemmed_word]
+    return list_of_stemmed_words
 
 
 if __name__ == "__main__":
@@ -143,5 +212,18 @@ if __name__ == "__main__":
 Pascal, CLU, Alphard, Russell, Ada, ALGOL 68, EL1.  List any other languages
 that are referenced frequently in papers on the above languages (e.g. catch
 any languages with interesting type structures that I might have missed)."""
-    result = vectorial_search(reversed_index, dic_doc, query_40, weight_tf_idf_query1, weight_tf_idf_doc1)
-    print([res[0] for res in result[:10]])
+
+    query_60 = """Hardware and software relating to database management systems.
+Database packages, back end computers, special associative hardware
+with microcomputers attached to disk heads or things like RAP, 
+relational or network (CODASYL) or hierarchical models, systems
+like SYSTEM R, IMS, ADABAS, TOTAL, etc."""
+
+   # print(reversed_index['algorithm'])
+    #print(list_of_words(1989,reversed_index, collection))
+    #result = vectorial_search(reversed_index, dic_doc, query_60, weight_tf_idf_query1, weight_tf_idf_doc1, collection)
+    #result2 = vectorial_search(reversed_index, dic_doc, query_60, weight_tf_idf_query2, weight_tf_idf_doc2, collection)
+    result3 = vectorial_search(reversed_index, dic_doc, query_60, weight_tf_idf_query3, weight_tf_idf_doc3, collection)
+    #print([res[0] for res in result[:10]])
+    #print([res[0] for res in result2[:10]])
+    print([res for res in result3[:10]])
